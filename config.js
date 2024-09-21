a62d0a9def0b74bcb59e0665cb085e2112da4df6
Margustr <mmargus960@gmail.com>
2024-09-21 07:48:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    token: "",
-    prefix: ".",
+    token: "MTI4NjkxMDc4NzQ3NDAzNDY5OA.GjZwPz.SXxcqx0FoRnXO_2VGWaFhWA9B3SRfVDKiRv2W0",
+    prefix: "!",
     botID: "",
-    ownerID: "",
+    ownerID: ""1286910787474034698,
     supportServer: "",
     website: "",
-    serverID: "",
+    serverID: "1263610528765575260",
     politika: "",
     destekçiRolID: "",
-    botDavet: "",
-    oynuyor: "e!help | venCode",
+    botDavet: "https://discord.com/oauth2/authorize?client_id=1286910787474034698&permissions=8&integration_type=0&scope=bot",
+    oynuyor: "!help | 7margusss",
     durum: "dnd"
 }
